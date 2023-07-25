8f057c00a49fae92f8872c0b68fff5b39a7b3e2a
Anant Chaudhary <139758290+anant996@users.noreply.github.com>
2023-07-25 17:03:31 +0530

    Updated server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
+from passPkg import generate_pass
 
 PORT = 8080
 
@@ -16,7 +17,7 @@ class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
             # Set the response content for the root URL
             response_content = "Welcome to the server!"
 
-            # Send the response content as bytes
+         # Send the response content as bytes
             self.wfile.write(response_content.encode('utf-8'))
 
         elif self.path == '/ping':
@@ -28,10 +29,25 @@ class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
 
             # Set the response content for /ping
-            response_content = "Pong"
+            response_content = "Ponggg"
 
             # Send the response content as bytes
             self.wfile.write(response_content.encode('utf-8'))
+
+        elif self.path == '/pwd':
+            # Set response status code
+            self.send_response(200)
+
+            # Set response headers
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+
+            # Generate a random password using a function from your package
+            generated_password = generate_pass()
+
+            # Send the generated password as the response
+            self.wfile.write(generated_password.encode('utf-8'))    
+
 
         else:
             # For any other URLs, send a 404 error response
